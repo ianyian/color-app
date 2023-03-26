78550243e4c9bb21abf906d75054a4e3aed82dc7
XJ <ianyian@gmail.com>
2023-03-26 19:33:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -19,24 +19,23 @@ color_codes = {
 
 #color = os.environ.get('APP_COLOR') or random.choice(["red","green"])
 #color = os.environ.get('APP_COLOR') or random.choice(["white"])
-#color = os.environ.get('APP_COLOR') or random.choice(["pink","blue","yellow"])
+color = os.environ.get('APP_COLOR') or random.choice(["pink","blue","yellow"])
 
 @app.route("/")
 def main():
     #return 'Hello'
-    #color = os.environ.get('APP_COLOR') or random.choice(["pink","blue","yellow"])
-    #print(color)
-    return render_template('hello.html', name=socket.gethostname(), color_codes["red"])
+    print(color)
+    return render_template('hello.html', name=socket.gethostname(), color=color_codes[color])
 
 @app.route('/color/<new_color>')
 def new_color(new_color):
-    return render_template('hello.html', name=socket.gethostname(), color_codes["red"])
+    return render_template('hello.html', name=socket.gethostname(), color=color_codes[new_color])
 
 @app.route('/read_file')
 def read_file():
     f = open("/data/testfile.txt")
     contents = f.read()
-    return render_template('hello.html', name=socket.gethostname(), contents=contents, color_codes["red"])
+    return render_template('hello.html', name=socket.gethostname(), contents=contents, color=color_codes[color])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="8080")
